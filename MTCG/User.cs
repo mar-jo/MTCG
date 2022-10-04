f282eea6f0d8c4ecfa4d40ac8c5cfb7f4cc4b043
marjo <markx931@gmail.com>
2022-10-04 21:55:47 +0200

    User Properties

---
+++ MTCG/User.cs
@@ -0,0 +1,19 @@
+﻿namespace MTCG;
+
+public class User
+{
+    public string Name { get; set; }
+    private string _password;
+
+    public string Password
+    {
+        get => _password;
+        set => _password = value.ToLower();
+    }
+
+    public User(string name, string password)
+    {
+        this.Name = name;
+        _password = password;
+    }
+}
