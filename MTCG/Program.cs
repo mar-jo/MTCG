cf950ce296dc40ca3517385656a1e8b949cb46ff
marjo <markx931@gmail.com>
2022-10-11 23:04:26 +0200

    fff

--- MTCG/Program.cs
+++ MTCG/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace MTCG;
 
-namespace MTCG 
-{
-    class Program
+class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
         }
     }
-}
 
--- MTCG/User.cs
+++ MTCG/User.cs
@@ -2,18 +2,45 @@
 
 public class User
 {
-    public string Name { get; set; }
+    public string username;
     private string _password;
+
+    public string Username
+    {
+        get
+        {
+            return this.username;
+        }
+        set
+        {
+            // Later on use SQL to check uniqueness of username
+            if (value.Length == 0)
+            {
+                throw new Exception("Username cannot be empty.");
+            }
+            this.username = value;
+        }
+    }
 
     public string Password
     {
-        get => _password;
-        set => _password = value.ToLower();
+        get
+        {
+            return this._password;
+        }
+        set
+        {
+            if (value.Length < 8)
+            {
+                throw new Exception("Password cannot be less than 8 characters.");
+            }
+            this._password = value;
+        }
     }
 
     public User(string name, string password)
     {
-        this.Name = name;
+        this.Username = name;
         _password = password;
-    }
+
 }
